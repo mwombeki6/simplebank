6abef6cdac292ce8e03c264add9006f1861c52a1
mwombeki6 <hackerlubex@protonmail.com>
2023-09-26 21:34:14 +0300

    import go package

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,16 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCreateAccount(t *testing.T) {
-	
+	arg := CreateAccountParams{
+		Owner:    "eri",
+		Balance:  590,
+		Currency: "TSH",
+	}
 }
