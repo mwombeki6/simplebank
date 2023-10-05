1eedf9edac2e68621117f3cbad9c585d4afea253
mwombeki6 <hackerlubex@protonmail.com>
2023-10-05 21:30:03 +0300

    commit

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -55,7 +55,7 @@ func TestUpdateAccount(t *testing.T) {
 		Balance: util.RandomMoney(),
 	}
 
-	account2, err := testStore.UpdateAccount(context.Background(), arg)
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, account2)
 
