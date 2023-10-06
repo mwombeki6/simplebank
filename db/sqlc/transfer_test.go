3fff9b613cae30b35b4b5911b7ff7a44a05b3dc6
mwombeki6 <hackerlubex@protonmail.com>
2023-10-06 18:50:43 +0300

    add createRandomTransfer

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -1,1 +1,31 @@
 package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/mwombeki6/simplebank/util"
+)
+
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID: account2.ID,
+		Amount: util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
