4dab6d2cef108a75f5154d522ca94a62677329c1
mwombeki6 <hackerlubex@protonmail.com>
2023-10-06 19:18:20 +0300

    add TestGetTransfer

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -5,8 +5,10 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	//"time"
+
 	"github.com/mwombeki6/simplebank/util"
+	"github.com/stretchr/testify/require"
 )
 
 func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
@@ -34,4 +36,20 @@ func TestCreateTransfer(t *testing.T) {
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
 	createRandomTransfer(t, account1, account2)
+}
+
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, account1, account2)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
 }
