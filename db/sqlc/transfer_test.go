5816cde1e93e6581c052804cbee6a854b42cbfd0
mwombeki6 <hackerlubex@protonmail.com>
2023-10-06 19:04:08 +0300

    add TestCreateTransfer

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -29,3 +29,9 @@ func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
 
 	return transfer
 }
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	createRandomTransfer(t, account1, account2)
+}
